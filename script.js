f2ab17a7d7cde81264af4c6488d07207af7202f8
pavan-lokesh <76544480+pavan-lokesh@users.noreply.github.com>
2021-06-04 13:33:01 +0530

    my version 1

---
+++ script.js
@@ -0,0 +1,64 @@
+ $(document).ready(function(){
+
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+
+        }
+        
+    });
+
+
+
+    // toggle menu/navbar script
+
+    $('menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active")
+    })
+
+
+    // typing animation script 
+
+    var typed = new Typed(".typing",{
+        strings: ["Developer",  "Student" , "Free lancer", "logo designer", "Editor"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    var typed = new Typed(".typing-2",{
+        strings: ["Developer",  "Student" , "Free lancer", "logo designer", "Editor"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    // owl carousel script 
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause:true,
+        Responsive: {
+
+            0:{
+                items:1,
+                nav: false
+            },
+
+            600:{
+                items:2,
+                nav: false
+            },
+
+            1000:{
+                items:3,
+                nav: false
+            }
+        }
+
+    })
+ });
