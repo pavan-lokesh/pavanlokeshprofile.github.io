118beeb42c76fd5f5e0eb1ad9af288a2314cbc14
kamal <kamalkoushik24@gmail.com>
2021-06-23 21:18:33 +0530

    final

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
 const { getMaxListeners } = require('process');
-
 function send(){
+  
+
+
     var nodemailer = require('nodemailer');
 
     const form = document.getElementById("contact-form");
